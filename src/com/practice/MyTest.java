62b8cdf99e3388d7f691455c9144e1ec8e52a4b7
corover <corover@com>
2020-08-24 20:01:59 +0530

    24-08-2020
    24-08-2020

--- src/com/practice/MyTest.java
+++ src/com/practice/MyTest.java
@@ -5,23 +5,23 @@ import java.io.IOException;
 
 class Derived1
 {
-    String s = "kishore";
+    String s ;
     Integer i;
     Derived1(){
 
 
     }
-   public  void display(){
-        System.out.println("String: " + s );
+   public static void display(String s){
+       // System.out.println("String: " + s );
         //String s = "kalempudi";
        System.out.println("output: " + s);
     }
     public static void main(String[] args) {
-        new Derived1();
-        Derived1 derived1 =new Derived1();
-       derived1.display();
+        new Derived1().display("kallempudi");
+       /* Derived1 derived1 =new Derived1();
+       derived1.display();*/
+        Derived1.display("kishore");
 
-       
     }
 }
 
